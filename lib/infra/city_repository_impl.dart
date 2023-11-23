54e1f9ac0f82e601278033d89aad576cccf66983
Welliton Klein <welliton.fokushima@gmail.com>
2023-11-23 17:27:59 -0300

    feat: aplicando cobertura de testes

--- lib/infra/city_repository_impl.dart
+++ lib/infra/city_repository_impl.dart
@@ -12,15 +12,21 @@ class CityRepositoryImpl implements CityRepository {
 
   @override
   Future<List<CityEntity>> searchByName({required String search}) async {
+    if (search.isEmpty) {
+      throw Exception('Nenhuma cidade informada na pesquisa');
+    }
+
     const apiKey = 'c93ec169bc7918636f32dc40e73d73d8';
     final url =
         'https://api.openweathermap.org/geo/1.0/direct?q=$search&limit=5&lang=pt_br&APPID=$apiKey';
+    final response = await client.get(Uri.parse(url));
 
-    if (search.isEmpty) {
-      throw Exception('Nenhuma cidade informada');
+    final cities = CityDto.fromJson(response.body);
+
+    if (cities.isEmpty) {
+      throw Exception('Nenhuma cidade foi encontrada');
     }
 
-    final response = await client.get(Uri.parse(url));
-    return CityDto.fromJson(response.body);
+    return cities;
   }
 }
