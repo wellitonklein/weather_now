10ace840bdeccda802bf2c995bfdc12164725aab
Welliton Klein <welliton.fokushima@gmail.com>
2023-12-11 16:52:58 -0300

    chore: add `device_preview`

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+import 'package:device_preview/device_preview.dart';
 
 import 'core/core.dart';
 
 void main() async {
-  runApp(const AppWidget());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const AppWidget(),
+    ),
+  );
 }
